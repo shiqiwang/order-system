230a664cf78e3e2f9c3d00469052037bcb1d5cb5
Shiqi Wang <Shiqi Wang>
2016-02-23 17:29:23 +0800

    点餐功能初步完成，后面把样式弄完

--- static/js/orderFood.js
+++ static/js/orderFood.js
@@ -30,7 +30,40 @@ define(["compatible"], function (compatible) {
     };
     //
     var orderListChange = function (targetEle){
-    
+        var name = targetEle.getAttribute("data-name");
+        var price = targetEle.getAttribute("data-price");
+        var orderList = document.getElementById("my-order-list");
+        var itemNodes = orderList.childNodes;
+        //判断当前添加的item是否已在订单中存在，如果存在，则只用加数量
+        for(var i = 1; i< itemNodes.length; i++) {
+            if(itemNodes[i].nodeName == "DIV" && itemNodes[i].getAttribute("data-name") == name) {
+                var itemNum = itemNodes[i].getAttribute("data-number");
+                itemNum = parseFloat(itemNum) + 1;
+                itemNodes[i].setAttribute("data-number", itemNum);
+                var itemNumDis = itemNodes[i].getElementsByClassName("itemNumDis")[0];
+                itemNumDis.innerHTML = itemNum;
+                var itemPrDis = itemNodes[i].getElementsByClassName("itemPrDis")[0];
+                itemPrice.innerHTML = parseFloat(price) * parseFloat(itemNum);
+                return;
+            }
+        }
+        //当前添加的item没有存在在订单中时，新建itemList
+        var orderItem = document.createElement("div");
+        orderItem.setAttribute("data-name", name);
+        orderItem.setAttribute("data-number", 1);
+        var itemName = document.createElement("span");
+        var itemNumber = document.createElement("span");
+        var itemPrice = document.createElement("span");
+        itemName.setAttribute("class", "itemNamDis");
+        itemName.innerHTML = name;
+        itemPrice.setAttribute("class", "itemPrDis");
+        itemPrice.innerHTML = price;
+        itemNumber.setAttribute("class", "itemNumDis");
+        itemNumber.innerHTML = 1;
+        orderItem.appendChild(itemName);
+        orderItem.appendChild(itemNumber);
+        orderItem.appendChild(itemPrice);
+        orderList.appendChild(orderItem);
     };
     return {
         getMyOrderHeaderPos: getMyOrderHeaderPos,
